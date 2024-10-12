a382f102fe786625de610a5e64a471b6f9ab98c6
mohammedmishalk <mishalnunu@gmail.com>
2024-10-12 15:42:52 +0530

    error fixed

--- backend/models/userDetails.js
+++ backend/models/userDetails.js
@@ -8,8 +8,8 @@ const userDetailsSchema = new mongoose.Schema({
     birthday: { type: Date, required: true },
     resident: { type: String, required: true }, // added 'resident' field
     working: { type: String, required: true },
-    salary: { type: Number, required: true },
-    companyname: { type: String, required: true }, // added 'companyname' field
+    salary: { type: Number },
+    companyname: { type: String }, // added 'companyname' field
     Bank: { type: String, required: true } // added 'Bank' field
 }, { timestamps: true });
 
