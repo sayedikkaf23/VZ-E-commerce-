cd69f72d6cf57bad33e104e8f89eb0237d1d04b5
mohammedmishalk <mishalnunu@gmail.com>
2024-10-16 19:01:55 +0530

    error fixed

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+// var cookieParser = require("cookie-parser");
+// var logger = require("morgan");
 var cors = require("cors"); // Import cors
 var connectDB = require("./db/db");
 require("dotenv").config();
@@ -39,11 +39,11 @@ app.use(
 );
 
 // Middleware setup
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, '../frontend/dist/frontend')));
+
 app.use('/uploads', express.static('uploads'));
 
 // Routes setup
@@ -51,19 +51,23 @@ app.use("/", indexRouter);
 app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/frontend', 'index.html'));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // send the error response
-  res.status(err.status || 500).json({ message: err.message });
-});
+//   // send the error response
+//   res.status(err.status || 500).json({ message: err.message });
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
