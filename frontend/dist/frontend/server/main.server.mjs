aa156eab6527652b52daa3ab4a9bf3a5facc333e
mohammedmishalk <mishalnunu@gmail.com>
2024-10-19 11:40:32 +0530

    Business Bank added Build Done

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JTWNBTVS.mjs";import"./chunk-6H6VSRAF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-YLCQD6ZL.mjs";import"./chunk-6H6VSRAF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
