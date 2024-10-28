e4d7c7e8a4d0a227aeaf3042d3c062b8df066134
mohammedmishalk <mishalnunu@gmail.com>
2024-10-28 16:44:30 +0530

    error fixed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { BusinessBankAccountComponent } from './business-bank-account/business-b
 import { MailMangamentForm2Component } from './mail-mangament-form-2/mail-mangament-form-2.component';
 import { MailMangamentShowDetailsComponent } from './mail-mangament-show-details/mail-mangament-show-details.component';
 import { CustomerManagementComponent } from './customer-management/customer-management.component';
+import { VirtualReceptionistComponent } from './virtual-receptionist/virtual-receptionist.component';
 
 
 @NgModule({
@@ -53,6 +54,7 @@ import { CustomerManagementComponent } from './customer-management/customer-mana
     MailMangamentForm2Component,
     MailMangamentShowDetailsComponent,
     CustomerManagementComponent,
+    VirtualReceptionistComponent,
     
   ],
   imports: [
---
+++ frontend/src/app/virtual-receptionist/virtual-receptionist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual-receptionist',
+  templateUrl: './virtual-receptionist.component.html',
+  styleUrl: './virtual-receptionist.component.css'
+})
+export class VirtualReceptionistComponent {
+
+}
