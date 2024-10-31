684776440bfe9374f2a033cba70125504d913589
mohammedmishalk <mishalnunu@gmail.com>
2024-10-31 15:46:53 +0530

    file upload

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4EKRRTN2.mjs";import"./chunk-6H6VSRAF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-L6AJT5VW.mjs";import"./chunk-6H6VSRAF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
