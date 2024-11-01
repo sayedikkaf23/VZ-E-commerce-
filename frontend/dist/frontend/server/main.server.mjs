6c2084c8ed7f0ed29bf700b3953dda426252769e
mohammedmishalk <mishalnunu@gmail.com>
2024-11-01 18:29:11 +0530

    salefoce added

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-L6AJT5VW.mjs";import"./chunk-6H6VSRAF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-S4TW77XU.mjs";import"./chunk-6H6VSRAF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
