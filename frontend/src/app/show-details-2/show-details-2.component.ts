8b0119048688e6795502d9da50d0e7531584237e
Shifana nasrin <shifananazrin15@gmail.com>
2024-11-07 11:39:12 +0530

    show details

--- frontend/src/app/show-details-2/show-details-2.component.ts
+++ frontend/src/app/show-details-2/show-details-2.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, AfterViewInit, Inject, PLATFORM_ID, HostListener } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr'; // For toast notifications
+import { UserService } from '../service/user.service';
+import { Router } from '@angular/router';
+import AOS from 'aos';
+import { switchMap } from 'rxjs';
+import { of } from 'rxjs';
+import Swal from 'sweetalert2';
+declare var $: any;
 
 @Component({
   selector: 'app-show-details-2',
   templateUrl: './show-details-2.component.html',
-  styleUrl: './show-details-2.component.css'
+  styleUrls: ['./show-details-2.component.css'] // Fixed styleUrls
 })
-export class ShowDetails2Component {
+export class ShowDetails2Component implements AfterViewInit { // Implement AfterViewInit interface
   isLoading = false;
 
+  isBrowser: boolean;
+ 
+  constructor(
+    private http: HttpClient,
+    private toastr: ToastrService, // For showing notifications
+    private router: Router,
+   
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId); // Check if the platform is a browser
+  }
+
+  ngOnInit(): void {
+
+
+   
+    
+  }
+  
+
+  ngAfterViewInit(): void {
+    if (this.isBrowser) {  // Ensure AOS and jQuery code runs only in the browser
+      AOS.init();
+
+      $(window).scroll(function () {
+        const height = $(window).scrollTop();
+        if (height > 50) {
+          $('html').addClass('sticky');
+        } else {
+          $('html').removeClass('sticky');
+        }
+      });
+
+      $(document).ready(() => {
+        $('.scrollToTop').click(function (event: any) {
+          event.preventDefault();
+          $('html, body').animate({ scrollTop: 0 }, 'slow');
+          return false;
+        });
+
+        $('.navbar-toggle').click(function () {
+          $('html').toggleClass('menu-show');
+        });
+
+        $('.header-menu-overlay').click(function () {
+          $('html').removeClass('menu-show');
+        });
+
+        $('.sub-menu-toggle').click(() => {
+          $(this).parent().toggleClass('submenu_active');
+        });
+      });
+    }
+  }
+
+  
 }
