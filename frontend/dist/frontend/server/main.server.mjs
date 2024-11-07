7bf88d6fa3f39f576eaf86618d9e9cc812544a20
mohammedmishalk <mishalnunu@gmail.com>
2024-11-07 16:33:02 +0530

    error fixed

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DIJXX74E.mjs";import"./chunk-XODVWN76.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-NUESU2IX.mjs";import"./chunk-XODVWN76.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
