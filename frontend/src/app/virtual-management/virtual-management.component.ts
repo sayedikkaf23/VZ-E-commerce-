1ab82b90a0c9a04e8cd29e6ce2b2e6f1c5baf478
Shifana nasrin <shifananazrin15@gmail.com>
2024-11-09 11:43:47 +0530

    virtual receiption in admin

--- frontend/src/app/virtual-management/virtual-management.component.ts
+++ frontend/src/app/virtual-management/virtual-management.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VirtualManagementService } from '../service/virtual-management.service';
-
+ 
 @Component({
   selector: 'app-virtual-management',
   templateUrl: './virtual-management.component.html',
@@ -10,13 +10,13 @@ export class VirtualManagementComponent implements OnInit {
   clientList: any[] = []; // To store fetched client data
   selectedClientDetails: any[] = []; // To store selected client's shareholder details
   showModal: boolean = false; // Flag to control modal visibility
-
+ 
   constructor(private virtualManagementService: VirtualManagementService) {}
-
+ 
   ngOnInit(): void {
     this.fetchClientDetails(); // Call the method when the component loads
   }
-
+ 
   fetchClientDetails(): void {
     this.virtualManagementService.getVirtaulData().subscribe(
       (response) => {
@@ -27,13 +27,15 @@ export class VirtualManagementComponent implements OnInit {
       }
     );
   }
-
+ 
   openClientDetails(shareholders: any[]): void {
+    console.log(shareholders)
     this.selectedClientDetails = shareholders; // Assign shareholder data to display in the modal
     this.showModal = true; // Open the modal
   }
-
+ 
   closeModal(): void {
     this.showModal = false; // Close the modal
   }
 }
+ 
