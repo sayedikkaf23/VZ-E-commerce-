4e3dea9a5f9ceef49c9b3cf13cd4de77d3c4c7f2
mohammedmishalk <mishalnunu@gmail.com>
2024-11-19 18:19:29 +0530

    bug fixed

--- backend/routes/onlinePayment.js
+++ backend/routes/onlinePayment.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const {
-    getPaymentModesHome,addAccountDetail,getAccountDetails,getSidebarData,getPiDataBySfId,payNow
+  getPaymentModesHome,
+  addAccountDetail,
+  getAccountDetails,
+  getSidebarData,
+  getPiDataBySfId,
+  payNow,
 
-    ,checkQuoteIdExists,payNowSaleforce,payNowByTelr,payNowByStripe,getPaymentModes,payNowByFiserv,updatePaymentModeStatus
+  checkQuoteIdExists,
+  payNowSaleforce,
+  payNowByTelr,
+  payNowByStripe,
+  getPaymentModes,
+  payNowByFiserv,
+  updatePaymentModeStatus,
 
-,getPaymentModeById,getPaymentMethodData,updatePaymentMethod,activatePaymentMethod,getPaymentMethods
+  getPaymentModeById,
+  getPaymentMethodData,
+  updatePaymentMethod,
+  activatePaymentMethod,
+  getPaymentMethods,
 } = require("../controllers/onlinePaymentController");
 const { authMiddleware } = require("../middleware/checkAuth");
 
@@ -19,18 +34,18 @@ router.get("/checkQuoteId/:quoteId", checkQuoteIdExists);
 router.post("/payNowSaleforce/:quoteId", payNowSaleforce);
 router.get("/payNowByStripe/:quoteId", payNowByStripe);
 router.get("/payNowByTelr/:quoteId", payNowByTelr);
-router.get("/getpayment", getPaymentModes);
+router.get("/get_payment_modes", getPaymentModes);
 router.get("/payNowByFiserv/:quoteId", payNowByFiserv);
 router.patch(
-    "/update_payment_mode_status",
-    authMiddleware,
-    updatePaymentModeStatus
-  );
-  router.get("/get_payment_mode/:id", authMiddleware, getPaymentModeById);
-  router.get("/get_payment_modes", authMiddleware, getPaymentModes);
-  router.put("/update_payment_method/:id", authMiddleware, updatePaymentMethod);
-  router.get("/get_payment_methods", authMiddleware, getPaymentMethodData);
-  router.post("/activatepay", activatePaymentMethod);
-  router.get("/getpayment", getPaymentMethods);
+  "/update_payment_mode_status",
+  authMiddleware,
+  updatePaymentModeStatus
+);
+router.get("/get_payment_mode/:id", authMiddleware, getPaymentModeById);
+router.get("/get_payment_modes", authMiddleware, getPaymentModes);
+router.put("/update_payment_method/:id", authMiddleware, updatePaymentMethod);
+router.get("/get_payment_methods", authMiddleware, getPaymentMethodData);
+router.post("/activatepay", activatePaymentMethod);
+router.get("/getpayment", getPaymentMethods);
 
 module.exports = router;
