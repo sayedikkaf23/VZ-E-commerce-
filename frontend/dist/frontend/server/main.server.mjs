635f7a7f4c57d51bc60d8bd074d438c1636ddf5d
Shifana nasrin <shifananazrin15@gmail.com>
2024-11-30 12:06:15 +0530

    file uplaod

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HXWSXIWW.mjs";import"./chunk-BBLLMXXU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-U6XAOFLS.mjs";import"./chunk-BBLLMXXU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
