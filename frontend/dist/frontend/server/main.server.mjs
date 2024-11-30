2928b784cf96d80f71483cd057ed0d2d03d99776
Shifana nasrin <shifananazrin15@gmail.com>
2024-11-30 10:22:38 +0530

    loader

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6FOR7YAZ.mjs";import"./chunk-BBLLMXXU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HOVV4PQE.mjs";import"./chunk-BBLLMXXU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
