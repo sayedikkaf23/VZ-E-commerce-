b235ebfb142c3bfa4e4f475d9cfec54030717bfc
mohammedmishalk <mishalnunu@gmail.com>
2024-12-05 13:51:16 +0530

    build

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,10 @@
-// const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
-// const saltRounds = 15;
 
-let user = new schema(
+const userSchema = new schema(
   {
-
-    email: { type: String, default: "" },
-
-    password: { type: String, default: "" },
-
+    email: { type: String, unique: true, required: true }, // Ensure email is unique
+    password: { type: String, required: true },
   },
   {
     strict: true,
@@ -17,29 +12,4 @@ let user = new schema(
   }
 );
 
-/* hash the password before save */
-// user.pre("save", function (next) {
-//   let user = this;
-//   // only hash the password if it has been modified (or is new)
-//   if (!this.isModified("password")) return next();
-//   // generate a salt
-//   bcrypt.genSalt(saltRounds, (err, salt) => {
-//     if (err) return next(err);
-//     // hash the password using our new salt
-//     bcrypt.hash(user.password, salt, (err, hash) => {
-//       if (err) return next(err);
-
-//       // override the cleartext password with the hashed one
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
-
-// /* user methods */
-// user.methods.comparePassword = async (password, hashPwsd) => {
-//   const match = await bcrypt.compare(password, hashPwsd);
-//   return match ? true : false;
-// };
-
-module.exports = mongoose.model("user", user);
+module.exports = mongoose.model("user", userSchema);
