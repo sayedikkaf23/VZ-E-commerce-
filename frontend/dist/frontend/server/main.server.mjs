be918bf51e677048fa0549005c6f5b30fe4ba505
Shifana nasrin <shifananazrin15@gmail.com>
2024-12-05 21:37:35 +0530

    oder summury

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DAO4BBPQ.mjs";import"./chunk-BBLLMXXU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-AYCSNFAD.mjs";import"./chunk-BBLLMXXU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
