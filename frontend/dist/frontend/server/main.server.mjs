909292031a3e6c3bdb497e10d0c5b2e99cee31e2
Shifana nasrin <shifananazrin15@gmail.com>
2025-01-01 11:27:48 +0530

    footer

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZX3HI44V.mjs";import"./chunk-47UK3NCL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-YVIB6D24.mjs";import"./chunk-47UK3NCL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
