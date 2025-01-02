0cde927160e6a7c8c98351c4f85867ea0604c974
mohammedmishalk <mishalnunu@gmail.com>
2025-01-02 10:06:55 +0530

    bug fixed

--- frontend/src/app/mail-mangament-form-2/mail-mangament-form-2.component.ts
+++ frontend/src/app/mail-mangament-form-2/mail-mangament-form-2.component.ts
@@ -72,14 +72,9 @@ export class MailMangamentForm2Component implements OnInit, AfterViewInit {
     //   this.cdRef.detectChanges(); // Manually trigger change detection to update the view
     // });
 
-    this.getnationalityService.getCountries().subscribe((data) => {
-      // Map and trim whitespace, sort case-insensitively
-      this.nationalities = data
-        .map((country: { name: { common: string } }) => country.name.common.trim())
-        .sort((a: string, b: string) => a.toLowerCase().localeCompare(b.toLowerCase()));
-    
-      // Trigger change detection to update the view
-      this.cdRef.detectChanges();
+    this.getnationalityService.getNationality().subscribe((data) => {
+      this.nationalities =  data.map((country: { name: { common: any; }; }) => country.name.common); // Get the Label values
+      this.cdRef.detectChanges(); // Trigger change detection to update the view
     });
 
 
