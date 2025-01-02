7f70e678593fe916746c2866cc5638af2aaf2e34
mohammedmishalk <mishalnunu@gmail.com>
2025-01-02 10:08:06 +0530

    bug fixed

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BPF2YY6S.mjs";import"./chunk-47UK3NCL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-PCUA43S5.mjs";import"./chunk-47UK3NCL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
