95a4c0b657a4bd3c3c86e9ae55fffe439cbe153b
nambi-thiru <nambidravid@gmail.com>
2025-01-16 12:03:14 +0530

    reverted the changes

--- frontend/src/app/customer-cardmanagement/customer-cardmanagement.component.ts
+++ frontend/src/app/customer-cardmanagement/customer-cardmanagement.component.ts
@@ -129,32 +129,32 @@ export class CustomerCardmanagementComponent implements OnInit {
     this.showModal = true;
   }
 
-  onFilesSelected(event: Event, index: number) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      // Get the selected files and create object URLs for each
-      const files = Array.from(input.files);
-      const fileUrls = files.map(file => ({
-        name: file.name,
-        url: URL.createObjectURL(file)
-      }));
+  // onFilesSelected(event: Event, index: number) {
+  //   const input = event.target as HTMLInputElement;
+  //   if (input.files && input.files.length > 0) {
+  //     // Get the selected files and create object URLs for each
+  //     const files = Array.from(input.files);
+  //     const fileUrls = files.map(file => ({
+  //       name: file.name,
+  //       url: URL.createObjectURL(file)
+  //     }));
   
-      // Add the selected files to the shareholder's uploadedFiles array
-      this.selectedShareholders[index].uploadedFiles = [
-        ...(this.selectedShareholders[index].uploadedFiles || []),
-        ...fileUrls
-      ];
+  //     // Add the selected files to the shareholder's uploadedFiles array
+  //     this.selectedShareholders[index].uploadedFiles = [
+  //       ...(this.selectedShareholders[index].uploadedFiles || []),
+  //       ...fileUrls
+  //     ];
   
-      // Optionally, clear the input field (to reset the input display)
-      // input.value = ''; // This will reset the file input to make it look like a fresh file selection
-    }
-  }
+  //     // Optionally, clear the input field (to reset the input display)
+  //     // input.value = ''; // This will reset the file input to make it look like a fresh file selection
+  //   }
+  // }
   
   
-  viewFile(fileUrl: string) {
-    // Open the file in a new browser tab
-    window.open(fileUrl, '_blank');
-  }
+  // viewFile(fileUrl: string) {
+  //   // Open the file in a new browser tab
+  //   window.open(fileUrl, '_blank');
+  // }
   
   
   
