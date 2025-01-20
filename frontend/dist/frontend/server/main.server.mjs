e914916f50b887395ee5708c41a897d7c0143fb2
Shifana nasrin <shifananazrin15@gmail.com>
2025-01-20 10:33:34 +0530

    api implemeted in frontend

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6YSZVXZS.mjs";import"./chunk-XZDK73XR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-X3NP2NGF.mjs";import"./chunk-XZDK73XR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
