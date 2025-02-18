3c092d169e942ad52ecff36d1bb8d3dbd4da9f62
nambi-thiru <nambidravid@gmail.com>
2025-02-18 12:05:23 +0530

    error fixing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -93,7 +93,7 @@ exports.forgotPassword = async (req, res) => {
       await user.save();
   
       // Send reset link via email
-      const resetUrl = `http://localhost:4200/reset-password?token=${resetToken}`;
+      const resetUrl = `https://virtuzone.yeepeey.com/reset-password?token=${resetToken}`;
   
       const transporter = nodemailer.createTransport({
         service: "Gmail",
