5c79d9436b16b926b0c8eed7702aff0252e230c7
Shifana nasrin <shifananazrin15@gmail.com>
2025-02-24 11:02:57 +0530

    font color for help center

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-M7OMGSWZ.mjs";import"./chunk-SKNTHOPP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-3ROFSPWP.mjs";import"./chunk-SKNTHOPP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
