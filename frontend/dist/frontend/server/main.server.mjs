f67a2b2f811fa13e444eaf9dfd5aacb10d1fe10c
mohammedmishalk <mishalnunu@gmail.com>
2025-02-24 10:33:36 +0530

    css error fixed

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZIA5G5AL.mjs";import"./chunk-SKNTHOPP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-6B4WQOKD.mjs";import"./chunk-SKNTHOPP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
