543f3d20429b4c7ac0ca8bfb1f2c571db69fd2a4
Shifana nasrin <shifananazrin15@gmail.com>
2025-03-05 04:40:04 +0530

    PPC-79 build

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-K2GHJRXH.mjs";import"./chunk-SKNTHOPP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-JRN5NKO2.mjs";import"./chunk-SKNTHOPP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
