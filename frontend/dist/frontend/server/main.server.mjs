15babeaf97db58f22ff97d72a8006913a671f119
mohammedmishalk <mishalnunu@gmail.com>
2025-03-10 15:04:59 +0530

    build

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TI6PM7LO.mjs";import"./chunk-WX2VT2NN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-WJ5C5DR5.mjs";import"./chunk-WX2VT2NN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
