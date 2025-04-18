2ec181b1e3f6baa9dc3528aa962fed6c8318feba
mohammedmishalk <mishalnunu@gmail.com>
2025-04-18 16:47:13 +0530

    build

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-C7CRI526.mjs";import"./chunk-FPSL7BAM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-YZU6IGYD.mjs";import"./chunk-FPSL7BAM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
