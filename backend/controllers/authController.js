ccf02e5455ce7d93ed673568cbbc951167cb1d1c
nambi-thiru <nambidravid@gmail.com>
2025-05-02 17:03:27 +0530

    PPC-208

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -142,7 +142,7 @@ exports.forgotPassword = async (req, res) => {
                     <table class="image_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
                       <tr>
                         <td style="padding-bottom: 20px; text-align: center;">
-                          <img src="https://res.cloudinary.com/dvekmmxxx/image/upload/v1718347384/photo_2024-06-14_11-46-14-removebg-preview_bml8en.png" style="max-width: 183px; width: 100%; height: auto; border: 0;" alt="Virtuzone Logo">
+                          <img src="https://res.cloudinary.com/dotkngkpl/image/upload/v1739944226/thumbnail_vz-ascentium_1_yrtbkn.png" style="max-width: 183px; width: 100%; height: auto; border: 0;" alt="Virtuzone Logo">
                         </td>
                       </tr>
                     </table>
