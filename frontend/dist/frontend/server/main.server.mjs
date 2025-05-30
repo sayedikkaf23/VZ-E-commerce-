b23056d1d0553c46d4df03d873664e2b16b4ee95
nambi-thiru <nambidravid@gmail.com>
2025-05-30 16:40:27 +0530

    build

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HG6WCMFM.mjs";import"./chunk-FRMCATOP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-B2KVLZPO.mjs";import"./chunk-FRMCATOP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
