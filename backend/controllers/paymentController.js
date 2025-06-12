3b526ee54a97c2f44991b26ca03c79992e089540
mohammedmishalk <mishalnunu@gmail.com>
2025-06-12 18:59:33 +0530

    build

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1922,7 +1922,7 @@ const AddCashCounter = async (req, res) => {
   //  console.log(quoteId)
 
   const tokenResponse = await axios.post(
-    `${process.env.EXTERNAL_API_SERVISE_URL}/services/oauth2/token?client_id=3MVG92u_V3UMpV.iJ_PYoQIn.oBrD2K8M5KXly5UByR5PJScjbzghqvSh4Q1bWn901ksE5yXQ1nCu2jBS20ip&client_secret=0FF7FF381C10DC1CCCA1479939F21AA2370A640CAAF8730B8E3E90A7793AE6E1&grant_type=password&username=vzpaymentapi@vz.ae.vzfullcopy&password=VZ@12345678`
+    `${process.env.EXTERNAL_API_SERVISE_URL}/services/oauth2/token?client_id=3MVG92u_V3UMpV.iJ_PYoQIn.oBrD2K8M5KXly5UByR5PJScjbzghqvSh4Q1bWn901ksE5yXQ1nCu2jBS20ip&client_secret=0FF7FF381C10DC1CCCA1479939F21AA2370A640CAAF8730B8E3E90A7793AE6E1&grant_type=password&username=vzpaymentapi@vz.ae.vzfullcopy&password=Virtuzone@1234`
   );
 
   const accessToken = tokenResponse.data.access_token;
