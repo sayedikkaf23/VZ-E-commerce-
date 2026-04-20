39208a877229dec1784d144c0e7b6dfdd2713421
mohammedmishalk <mishalnunu@gmail.com>
2026-04-20 16:32:25 +0530

    fav icon

--- frontend/server.ts
+++ frontend/server.ts
@@ -1,18 +1,14 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
-import AppServerModule from './src/main.server';
+import { existsSync } from 'node:fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
-
-  const commonEngine = new CommonEngine();
+  const indexHtml = join(browserDistFolder, 'index.html');
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -23,28 +19,15 @@ export function app(): express.Express {
     next();
   });
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('**', express.static(browserDistFolder, {
     maxAge: '1y',
     index: 'index.html',
   }));
 
-  // All regular routes use the Angular engine
-  server.get('**', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
-    commonEngine
-      .render({
-        bootstrap: AppServerModule,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+  // All other routes: serve index.html (SPA client-side routing)
+  server.get('**', (req, res) => {
+    res.sendFile(indexHtml);
   });
 
   return server;
